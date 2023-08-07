654fc3c7eefa801ed140a83482ea1c34e91b9f4c
Jacob <jacobphelan@hotmail.com>
2023-08-07 14:41:59 +1000

    feat: Add Registrant#eligible_for_permit?

--- lib/registrant.rb
+++ lib/registrant.rb
@@ -5,18 +5,22 @@ class Registrant
 
   attr_accessor :license_data
 
-  def initialize(name, age, permit= false, license_data= {written: false, license: false, renewed: false})
-	  @name = name
+	def initialize(name, age, permit= false, license_data= {written: false, license: false, renewed: false})
+		@name = name
 		@age = age
 		@permit = permit
 		@license_data = license_data
 	end
 
 	def permit?
-	  return @permit
+		return @permit
 	end
 
 	def earn_permit
-	  @permit = true
+		@permit = true
+	end
+
+	def eligible_for_permit?
+		age >= 16
 	end
 end
