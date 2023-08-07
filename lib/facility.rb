cd721c6aac7ebcf3ab51d4379eb570d0e8c93eb0
Jacob <jacobphelan@hotmail.com>
2023-08-07 13:16:47 +1000

    feat: Add #register_vehicle & tests

--- lib/facility.rb
+++ lib/facility.rb
@@ -2,16 +2,24 @@ class Facility
   attr_reader :name,
               :address,
               :phone,
-              :services
+              :services,
+							:registered_vehicles,
+							:collected_fees
 
   def initialize(facility_information)
     @name = facility_information[:name]
     @address = facility_information[:address]
     @phone = facility_information[:phone]
     @services = []
+		@registered_vehicles = []
+		@collected_fees = 0
   end
 
   def add_service(service)
     @services << service
+  end
+
+  def register_vehicle(vehicle)
+		@registered_vehicles << vehicle
   end
 end
