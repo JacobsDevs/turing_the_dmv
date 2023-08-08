03a8382374af03b110ee8867782b5423b89c3f25
Jacob <jacobphelan@hotmail.com>
2023-08-08 15:08:08 +1000

    feat: Add FacilityFactory#initialize

---
+++ lib/facility_factory.rb
@@ -0,0 +1,7 @@
+class FacilityFactory
+  attr_reader :factory_list
+	
+	def initialize
+	  @factory_list = []
+	end
+end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -2,14 +2,14 @@ require 'spec_helper'
 
 RSpec.describe FacilityFactory do
   before(:each) do
-	  factory = FacilityFactory.new
+	  @factory = FacilityFactory.new
 	end
 	describe '#initialize' do
 		it 'can initialize' do
-			expect(factory).to be_an_instance_of(FacilityFactory)
+			expect(@factory).to be_an_instance_of(FacilityFactory)
 		end
 		it 'initializes with an empty list of factories' do
-		  expect(factory.factory_list).to eq([])
+		  expect(@factory.factory_list).to eq([])
 	  end
 	end
 end
