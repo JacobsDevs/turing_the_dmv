0f78e8e8d063dfa63ea5d15f8d27f8fa5869fbda
Jacob <jacobphelan@hotmail.com>
2023-08-08 15:13:35 +1000

    test: Add RSpec for FacilityFactory#create_facilities

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 RSpec.describe FacilityFactory do
   before(:each) do
 	  @factory = FacilityFactory.new
+		@colorado_facilities = DmvDataService.new.co_dmv_office_locations
 	end
 	describe '#initialize' do
 		it 'can initialize' do
@@ -11,5 +12,11 @@ RSpec.describe FacilityFactory do
 		it 'initializes with an empty list of factories' do
 		  expect(@factory.factory_list).to eq([])
 	  end
+	end
+  describe '#create_facilities' do
+		it 'fills factory_list with Factory objects for Colorado format' do
+			factory.create_facilities(@colorado_facilities)
+			expect(@factory.factory_list[0]).to be_an_instance_of(Facility)
+		end
 	end
 end
