d4a7b313d7beb16a288972d816e80cdc78b1dba8
Jacob <jacobphelan@hotmail.com>
2023-08-08 15:25:14 +1000

    test: Add FacilityFactory#format_services

--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -19,4 +19,9 @@ RSpec.describe FacilityFactory do
 			expect(@factory.factory_list[0]).to be_an_instance_of(Facility)
 		end
 	end
+	describe '#format_services' do
+		it 'returns a formatted array of services for use with the Facility class' do
+		  expect(factory.format_services(["renewals; license"])).to eq(["Renew License"])
+		end
+	end
 end
