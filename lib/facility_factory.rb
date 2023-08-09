d0d68a9b4e26390339fd06abcbc9b6050b48dc4c
Jacob <jacobphelan@hotmail.com>
2023-08-09 14:05:27 +1000

    feat: Add FacilityFactory#format_name

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -7,4 +7,20 @@ class FacilityFactory
 	  @factory_list = []
 	end
 
+	def create_facilities(dataset)
+		dataset.each do |data|
+			data = format_data(data)
+			p data[:name]
+		end
+		dataset
+	end
+
+	def format_data(data)
+		data[:name] = format_name(data)
+		data
+	end
+
+	def format_name(data)
+	  string = data[:dmv_office] + ", " + data[:state]
+	end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -14,16 +14,22 @@ RSpec.describe FacilityFactory do
 	  end
 	end
 	describe '#create_facilities' do
-		it 'read data from data set and sends it to #format_data'
+		it 'read data from data set and sends it to #format_data' do
+			expect(@factory.create_facilities(@colorado_facilities)[0][:name]).to eq("DMV Tremont Branch, CO")
+		end
 	end
 	describe '#format_data' do
-		it 'sends data to #format_name'
+		it 'sends data to #format_name' do
+		  expect(@factory.format_data({:dmv_office=>"DMV Test", :state=>"CO"})[:name]).to eq("DMV Test, CO")
+		end
 		it 'sends data to #format_address'
 		it 'sends data to #format_phone'
 		it 'sends data to #format_services'
 	end
 	describe '#format_name' do
-		it 'returns data in the format "DMV *LOCATION*, *STATE*"'
+		it 'returns string in the format "DMV *LOCATION*, *STATE*"' do
+		  expect(@factory.format_name({:dmv_office=>"DMV Test", :state=> "CO"})).to eq("DMV Test, CO")
+		end
 	end
 	describe '#format_address' do
 		it 'returns data in the format "#### Street, Suite ###, City, STATE, zip"'
