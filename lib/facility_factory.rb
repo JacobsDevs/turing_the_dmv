99bd436137af6722ba5e63591fac84f85586b137
Jacob <jacobphelan@hotmail.com>
2023-08-09 13:28:27 +1000

    rollback: back to start of FacilityFactory class, different approach

--- lib/facility_factory.rb
+++ lib/facility_factory.rb
@@ -7,29 +7,4 @@ class FacilityFactory
 	  @factory_list = []
 	end
 
-  def create_facilities(dataset)
-		dataset.each do |data|
-			data = format_data(data)
-			@factory_list << Facility.new(data)
-		end
-		p @factory_list
-	end
-
-  def format_data(data)
-    data[:name] = format_name(data)
-    data[:services] = format_services(data)
-		return data
-	end
-
-	def format_name(data)
-	  name = data[:dmv_office]
-	end
-
-	def format_services(data)
-		services = data[:services_p].split(",")
-	  formatted_services = services.map do |service|
-      "Vehicle Registration" if services == "registration"
-		end
-		return formatted_services
-	end
 end
--- spec/facility_factory_spec.rb
+++ spec/facility_factory_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe FacilityFactory do
   before(:each) do
 	  @factory = FacilityFactory.new
 		@colorado_facilities = DmvDataService.new.co_dmv_office_locations
-		@dirty_data = {dmv_office: "DMV Tremont Branch", services_p: "vehicle titles, registration, renewals; VIN inspections", phone: "758-000"}
-	  @clean_data = {name: "DMV Tremont Branch", services: ["vehicle titles", "Vehicle Registration", "renewals; VIN inspections"], phone: "758-000"}
+		@dirty_data = {dmv_office: "DMV Tremont Branch", services_p: "vehicle titles, registration, renewals;", phone: "758-000"}
+	  @clean_data = {name: "DMV Tremont Branch", services: ["vehicle titles", "Vehicle Registration", "Renew License"], phone: "758-000"}
 	  @facility = Facility.new(@clean_data)
 	end
 	describe '#initialize' do
@@ -16,26 +16,5 @@ RSpec.describe FacilityFactory do
 		  expect(@factory.factory_list).to eq([])
 	  end
 	end
-  describe '#create_facilities' do
-		it 'fills factory_list with Factory objects for Colorado format' do
-			@factory.create_facilities(@colorado_facilities)
-			expect(@factory.factory_list[0]).to be_an_instance_of(Facility)
-		end
-	end
-	describe '#format_data' do
-	  it 'returns formatted data for use with the Facility class' do
-			  data = @factory.format_data(@dirty_data)
-			expect(Facility.new(data)).to eq(@facility)
-		end
-	end
-	describe '#format_name' do
-		it 'returns a formatted name for use with the Facility class' do
-		  expect(@factory.format_name(@dirty_data)).to eq("DMV Tremont Branch")
-		end
-	end
-	describe '#format_services' do
-		it 'returns a formatted array of services for use with the Facility class' do
-		  expect(@factory.format_services(["renewals; license"])).to eq(["Renew License"])
-		end
-	end
+  
 end
